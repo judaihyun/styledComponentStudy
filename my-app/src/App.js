950a43cc346483ec49453016d0e51aaf483ff762
daihyun <judaihyun1@gmail.com>
2019-12-22 00:05:46 +0900

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { createGlobalStyle ,keyframes } from 'styled-components';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
 	body{
@@ -8,54 +9,45 @@ const GlobalStyle = createGlobalStyle`
 	}
 `;
 
-function App() {
-  return (
-	<Container>
-	  <Button>Hello</Button>
-	  <Button danger>Hello</Button>
-	  <Anchor href="http://google.com">go to go</Anchor>
-	</Container>
-  );
-}
+
+
+const Card = styled.div`
+	background-color:white;
+`;
+
 
 const Container = styled.div`
 	height:100vh;
 	width:100%;
 	background-color:pink;
+	${Card}{
+		background-color: blue;
+	}
 `;
+
+
 
 const Button = styled.button`
-	border-radius: 50px;
-	padding:5px;
-	min-width: 120px;
-	color:white;
-	font-weight: 600;
-	-webkit-appearance: none;
-	cursor:pointer;
-	&:active,
-	&:focus{
-		outline:none;
-	}
-	background-color: ${props => props.danger ? "#e74c3c" : "#2ecc71"};
-	${props => {
-		if(props.danger){
-			return `animation: ${rotation} 2s linear infinite`;
-		}
-	}};
+	border-radius: 30px;
+	padding: 25px 15px;
+	background-color: ${props => props.theme.successColor}
 `;
 
-const Anchor = styled(Button.withComponent("a"))`
-	text-decoration:none;
-`;
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+		<Container>
+			<Form/>
+		</Container>
+		</ThemeProvider>
+	);
+}
 
-const rotation = keyframes`
-	from{
-		transform: rotate(0deg);
-	}
-	to{
-		transform: rotate(360deg);
-	}
-`;
+const Form = () => (
+	<Card>
+		<Button>Hello</Button>
+	</Card>
+);
 
 
 export default App;
---
+++ my-app/src/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+	mainColor: '#2ecc71',
+	dangerColor: '#c0392b',
+	successColor: '#f1c40f',
+};
+
+
+export default theme;
